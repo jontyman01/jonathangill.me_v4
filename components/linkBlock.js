5d3e1d158803f315aa11d4049f134e8aa86d06cc
Jonathan Gill <hello@jonathangill.me>
2022-08-08 00:04:25 +0100

    polished linkBlock, spacing, wording

--- components/linkBlock.js
+++ components/linkBlock.js
@@ -1,14 +1,46 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function LinkBlock({ destination, a11yText, title, textColorClasses, bgColorClasses, num, company, skill_1 }) {
-    return (
-        <Link href={destination}>
-            <a aria-label={a11yText} className={`block content mb-4 p-4 rounded-md ${bgColorClasses}`}>
-                <p>{num}</p>
-                <h2 className={`text-transparent bg-clip-text bg-gradient-to-r ${textColorClasses}`}>{title}</h2>
-                <p>{company}</p>
-                <span class='rounded-full bg-gray-600 p-1'>{skill_1}</span>
-            </a>
-        </Link>
-    )
+export default function LinkBlock({
+  destination,
+  a11yText,
+  title,
+  textColorClasses,
+  bgColorClasses,
+  num,
+  company,
+  skill_1,
+}) {
+  return (
+    <Link href={destination}>
+      <a
+        aria-label={a11yText}
+        className={`block mb-4 p-4 rounded-md linkBlock relative ${bgColorClasses}`}
+      >
+        <p className="text-gray-400 pb-0 linedText relative pr-16">
+          <span class="bg-gray-800 pr-4">{num}</span>
+        </p>
+        <hr class="-mt-3 h-px border-b border-gray-500 bg-gray-500 transition ease-in-out delay-150" />
+        <p
+          className={`text-transparent text-3xl pt-4 pr-16 font-bold bg-clip-text bg-gradient-to-r ${textColorClasses}`}
+        >
+          {title}
+        </p>
+        <p className="pb-4">{company}</p>
+        <span class="rounded-full bg-gray-600 py-1 px-4">{skill_1}</span>
+        <div class="absolute inset-y-0 right-0 w-16 flex items-center" >
+          <svg
+            width="20"
+            height="24"
+            viewBox="0 0 20 24"
+            fill="none"
+            stroke-width="2"
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-gray-500 transition ease-in-out delay-150"
+          >
+            <path d="M1 1.60769L19 12L1 22.3923L1 1.60769Z" stroke="inherit" />
+          </svg>
+        </div>
+      </a>
+    </Link>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -27,48 +27,63 @@ export default function Home() {
           <Container>
             <m.article variants={fade}>
               <div className="md:flex flex-row pb-4">
-                <div className="md:mr-2 rounded-md basis-1/1 md:basis-2/3 bg-gradient-to-r from-green-800 to-blue-900 p-6 mb-4 md:mb-0 flex items-center">
+                <div className="md:mr-2 rounded-md basis-1/1 md:basis-2/3 bg-gradient-to-r from-green-800 to-blue-900 p-4 md:p-6 mb-4 md:mb-0 flex items-center">
                   <p className="text-3xl">
-                    With nearly a decade of experience, I aim to deliver
-                    customer centred digital designs tailored to their needs,
-                    and backed up by clear rationale.
+                    Hi, I'm Jon 👋 I have nearly a decade of experience
+                    designing and delivering bespoke, user focussed digital
+                    products, backed up by clear rationale.
                   </p>
                 </div>
-                <div className="md:ml-2 rounded-md basis-1/1 md:basis-1/3 bg-gradient-to-r from-slate-800 to-slate-800 p-6">
-                <p className="text-gray-400 pb-2">I've Worked With</p>
-                <p className="text-4xl font-bold">
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-red-500 to-red-900 pr-2">SPEEDO</span>
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-yellow-400 to-yellow-500 pr-2">WAER SYSTEMS</span>
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-red-400 to-blue-600 pr-2">BERGHAUS</span>
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-red-600 to-red-900 pr-2">SIG</span>
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-orange-400 to-red-700 pr-2">ELLESSE</span>
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-green-600 to-emerald-500 pr-2">RIGHTINDEM</span>
-                </p>
-                <p className="pt-2">+ Many More</p>
+
+                <div className="md:ml-2 rounded-md basis-1/1 md:basis-1/3 bg-gradient-to-r from-slate-800 to-slate-800 p-4 md:p-6">
+                  <p className="text-gray-400 pb-2">I've Worked With</p>
+                  <p className="text-4xl font-bold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-red-500 to-red-900 pr-2">
+                      SPEEDO
+                    </span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-yellow-400 to-yellow-500 pr-2">
+                      WAER SYSTEMS
+                    </span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-red-400 to-blue-600 pr-2">
+                      BERGHAUS
+                    </span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-red-600 to-red-900 pr-2">
+                      SIG
+                    </span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-orange-400 to-red-700 pr-2">
+                      ELLESSE
+                    </span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r inline-block from-green-600 to-emerald-500 pr-2">
+                      RIGHTINDEM
+                    </span>
+                  </p>
+                  <p className="pt-2">+ Many More</p>
                 </div>
               </div>
 
-              <div className="mb-4 p-4 rounded-md bg-gradient-to-r from-gray-800 to-purple-900">
-              <p className="text-gray-400 pb-2">Specializing In</p>
-                <Collapsible trigger="UI DESIGN &amp; DESIGN SYSTEMS" transitionTime="150" >
-                <p className="pb-4">
-                    Stuff about design systems
-                  </p>
+              <div className="mb-4 p-4 md:p-6 rounded-md bg-gradient-to-r from-gray-800 to-purple-900">
+                <p className="text-gray-400 pb-4">Specializing In</p>
+                <Collapsible
+                  trigger="UI DESIGN &amp; DESIGN SYSTEMS"
+                  transitionTime="150"
+                >
+                  <p className="pb-4">Stuff about design systems</p>
                 </Collapsible>
 
-                <Collapsible trigger="REQUIREMENTS GATHERING &amp; WIREFRAMING" transitionTime="150">
-                  <p className="pb-4">
-                    Stuff about design systems
-                  </p>
+                <Collapsible
+                  trigger="REQUIREMENTS GATHERING &amp; WIREFRAMING"
+                  transitionTime="150"
+                >
+                  <p className="pb-4">Stuff about design systems</p>
                 </Collapsible>
 
-                <Collapsible trigger="BESPOKE HTML &amp; CSS" transitionTime="150">
-                <p className="pb-4">
-                    Stuff about design systems
-                  </p>
+                <Collapsible
+                  trigger="BESPOKE HTML &amp; CSS"
+                  transitionTime="150"
+                >
+                  <p className="pb-4">Stuff about design systems</p>
                 </Collapsible>
               </div>
-
 
               <LinkBlock
                 destination="/waer-systems"

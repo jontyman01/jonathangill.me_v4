618d5a2685c404bdcacfb7ab0941983eb74a86d8
Jonathan Gill <hello@jonathangill.me>
2022-08-16 23:17:25 +0100

    fixed favicons

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,15 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
           <meta name="theme-color" content="#111827" />
-          <link rel="manifest" href="%PUBLIC_URL%/favicons/manifest.json"/>
+          <link rel="manifest" href="https://jonathangill.me/favicons/manifest.json"/>
 
-          <link rel="apple-touch-icon" sizes="180x180" href="%PUBLIC_URL%/favicons/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="%PUBLIC_URL%/favicons/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="%PUBLIC_URL%/favicons/favicon-16x16.png"/>
-          <link rel="manifest" href="%PUBLIC_URL%/favicons/site.webmanifest"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="https://jonathangill.me/favicons/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="https://jonathangill.me/favicons/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="https://jonathangill.me/favicons/favicon-16x16.png"/>
+          <link rel="manifest" href="https://jonathangill.me/favicons/site.webmanifest"/>
           <meta name="theme-color" content="#111827"/>
-          <link rel="shortcut icon" href="%PUBLIC_URL%/favicons/favicon.ico"/>
+          <link rel="shortcut icon" href="https://jonathangill.me/favicons/favicon.ico"/>
         </Head>
         
         <body className="bg-gray-100 dark:bg-gray-900 dark:text-slate-50">

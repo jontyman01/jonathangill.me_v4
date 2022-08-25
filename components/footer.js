8e53d32add567a3290b302e124f2e6c388c9f1e3
Jonathan Gill <Jono@Jonathans-MacBook-Pro-2.local>
2022-08-25 11:24:43 +0100

    fixed footer spacing

--- components/footer.js
+++ components/footer.js
@@ -10,14 +10,14 @@ export default function Footer() {
       <Container>
         <div className="py-4 bg-gray-800 p-4 md:p-6 rounded-md my-4">
           <div className="flex flex-wrap">
-            <div className="flex space-x-1 mb-1 md:mb-0">
-              <a href="https://www.linkedin.com/in/gilljonathan" target="_blank"><AiFillLinkedin className='inline' /> <span className="hover:underline decoration-2 hover:decoration-blue-400 focus:text-gray-500">LinkedIn</span></a>
-              <span className="block px-3 text-gray-400">&bull;</span>
+            <div className="md:flex mb-1 md:mb-0">
+              <a href="https://www.linkedin.com/in/gilljonathan" target="_blank" className='pb-1 md:pb-0 block md:inline'><AiFillLinkedin className='inline' /> <span className="hover:underline decoration-2 hover:decoration-blue-400 focus:text-gray-500">LinkedIn</span></a>
+              <span className="md:block px-3 text-gray-400 hidden md:visible">&bull;</span>
 
-              <a href='https://twitter.com/jontyman01' target="_blank"><span className="block"><AiFillTwitterCircle className='inline' /> <span className='hover:underline hover:decoration-cyan-500 decoration-2 focus:text-gray-500'>Twitter</span></span></a>
-              <span className="block px-3 text-gray-400">&bull;</span>
+              <a href='https://twitter.com/jontyman01' target="_blank" className='pb-1 md:pb-0 block md:inline'><span className="block"><AiFillTwitterCircle className='inline' /> <span className='hover:underline hover:decoration-cyan-500 decoration-2 focus:text-gray-500'>Twitter</span></span></a>
+              <span className="md:block px-3 text-gray-400 hidden md:visible">&bull;</span>
 
-              <a href='https://github.com/jontyman01/jonathangill.me_v4' target="_blank"><span className="block"><AiFillGithub className='inline' /> <span className='hover:underline hover:decoration-cyan-500 decoration-2 focus:text-gray-500'>View My Source Code</span></span></a>
+              <a href='https://github.com/jontyman01/jonathangill.me_v4' target="_blank" className='pb-1 md:pb-0 block md:inline'><span className="block"><AiFillGithub className='inline' /> <span className='hover:underline hover:decoration-cyan-500 decoration-2 focus:text-gray-500'>View My Source Code</span></span></a>
 
             </div>
 

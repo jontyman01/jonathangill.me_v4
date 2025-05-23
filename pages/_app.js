c3946b9c29783c17d597c6bad6fcbfa2500102fd
jontyman01 <jonogill@gmail.com>
2025-05-23 19:59:37 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,14 @@ import { useRouter } from 'next/router'
 import { ThemeProvider } from "next-themes";
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
 
   return (
     <>
+      <GoogleAnalytics trackPageViews gaMeasurementId="G-V9D5TC114G"/>
       <DefaultSeo {...SEO} /> 
       <ThemeProvider enableSystem={true} attribute="class">
       <AnimatePresence exitBeforeEnter>
